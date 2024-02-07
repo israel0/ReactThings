5d1e2ebb62633fc1a87688ae5e55830352044fc3
israel0 <seyeakinsola@gmail.com>
2024-02-07 16:15:46 +0100

    Css styling and Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-
+const isPurchased = false;
 function App() {
   return (
     <div className="App">
-            <Book title="Rich Dad Poor Dad" description="This is a book from my finance hub" />
-            <Book title="Rich Dad Poor Dad 2" description="This is a book from my finance hub" />
-            <Book title="Rich Dad Poor Dad 3" description="This is a book from my finance hub" />
-            <Book title="Rich Dad Poor Dad 4" description="This is a book from my finance hub" />
+            <Book title="Rich Dad Poor Dad" description="This is a book from my finance hub" purchased_date={isPurchased ? "January 2" : ""} />
+            <Book title="Rich Dad Poor Dad 2" description="This is a book from my finance hub" purchased_date={isPurchased ? "January 2" : ""}/>
+            <Book title="Rich Dad Poor Dad 3" description="This is a book from my finance hub" purchased_date={isPurchased ? "January 2" : ""}/>
+            <Book title="Rich Dad Poor Dad 4" description="This is a book from my finance hub" purchased_date={isPurchased ? "January 2" : ""}/>
     </div>
   );
 }
@@ -16,7 +16,7 @@ function App() {
 const Book = (props) => {
   return (
          <>
-             <h1>{props.title}</h1><p>{props.description}</p>
+             <h1 className={isPurchased ? "colorRed" : "Black"}>{props.title}</h1><p>{props.purchased_date}</p> <p style={{ color: "gold" }}>{props.description}</p> <button>{isPurchased ? "Read Now" : ""}</button>
          </>
   );
 }
